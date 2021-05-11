144cd786bc28a75f4ddc22dabe2d119eab00a6ab
halimyr8 <haeltretze@gmail.com>
2021-05-11 12:34:07 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+apt install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt update
+apt install auditbeat
+systemctl enable auditbeat
